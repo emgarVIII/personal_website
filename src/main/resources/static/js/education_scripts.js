81880613f8adb4f3c9dacc3dbfa5598e33aa75b3
Mauricio Garcia <mauriciogarcia@Mauricios-MacBook-Pro.local>
2023-07-06 03:05:36 -0500

    finished contact me, working on educ

---
+++ src/main/resources/static/js/education_scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const collapsibleHeadings = document.querySelectorAll(".collapsible-heading");
+  
+    collapsibleHeadings.forEach(function(heading) {
+      heading.addEventListener("click", function() {
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    });
+  });
+  
