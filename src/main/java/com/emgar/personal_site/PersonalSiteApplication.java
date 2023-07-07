d2a974aae2b4d16a0f3feb3259ab62a85746be85
Mauricio Garcia <mauriciogarcia@Mauricios-MacBook-Pro.local>
2023-07-06 21:27:57 -0500

    ui changes

--- src/main/java/com/emgar/personal_site/PersonalSiteApplication.java
+++ src/main/java/com/emgar/personal_site/PersonalSiteApplication.java
@@ -2,7 +2,7 @@ package com.emgar.personal_site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
